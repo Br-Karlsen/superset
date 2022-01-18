c5c830f0a48d59fd414c746f18e1a480b69284f1
Marcos Alcozer <marcos@alcozer.dev>
2022-01-18 17:31:17 +0000

    Adds feature flag for native filtering

--- src/superset_config.py
+++ src/superset_config.py
@@ -2,6 +2,18 @@ import os
 
 from flask_appbuilder.security.manager import AUTH_OAUTH
 
+
+ENABLE_PROXY_FIX=True
+ROW_LIMIT=5000
+SECRET_KEY=os.getenv("SECRET_KEY")
+SQLALCHEMY_DATABASE_URI=os.getenv("CONNECTION_STRING")
+WTF_CSRF_ENABLED=True
+CSRF_ENABLED=True
+
+FEATURE_FLAGS = {
+    "DASHBOARD_NATIVE_FILTERS": True,
+}
+
 AUTH_TYPE=AUTH_OAUTH
 AUTH_USER_REGISTRATION=True
 AUTH_USER_REGISTRATION_ROLE="Public"
@@ -21,10 +33,3 @@ OAUTH_PROVIDERS = [{
         "authorize_url": "https://accounts.google.com/o/oauth2/auth"
     }
 }]
-
-ENABLE_PROXY_FIX=True
-ROW_LIMIT=5000
-SECRET_KEY=os.getenv("SECRET_KEY")
-SQLALCHEMY_DATABASE_URI=os.getenv("CONNECTION_STRING")
-WTF_CSRF_ENABLED=True
-CSRF_ENABLED=True

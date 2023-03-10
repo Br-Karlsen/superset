a164a3213c7d15132ab7bb9a7b1fd08a109dd355
Magnus Fagertun <magnus.fagertun@gmail.com>
2023-03-10 15:19:19 +0100

    added groups for auth_role_mapping

--- src/superset_config.py
+++ src/superset_config.py
@@ -2,34 +2,46 @@ import os
 
 from flask_appbuilder.security.manager import AUTH_OAUTH
 
-
-ENABLE_PROXY_FIX=True
-ROW_LIMIT=5000
-SECRET_KEY=os.getenv("SECRET_KEY")
-SQLALCHEMY_DATABASE_URI=os.getenv("CONNECTION_STRING")
-WTF_CSRF_ENABLED=True
-CSRF_ENABLED=True
+ENABLE_PROXY_FIX = True
+ROW_LIMIT = 5000
+SECRET_KEY = os.getenv("SECRET_KEY")
+SQLALCHEMY_DATABASE_URI = os.getenv("CONNECTION_STRING")
+WTF_CSRF_ENABLED = True
+CSRF_ENABLED = True
 
 FEATURE_FLAGS = {
     "DASHBOARD_NATIVE_FILTERS": True,
 }
 
-AUTH_TYPE=AUTH_OAUTH
-AUTH_USER_REGISTRATION=True
-AUTH_USER_REGISTRATION_ROLE="Admin"
+AUTH_TYPE = AUTH_OAUTH
+AUTH_USER_REGISTRATION = True
+AUTH_USER_REGISTRATION_ROLE = "Gamma"
 
-OAUTH_PROVIDERS = [{
-    "name": "google",
-    #"whitelist": ["@company.com"],
-    "icon": "fa-google",
-    "token_key": "access_token",
-    "remote_app": {
-        "client_id": os.getenv("GOOGLE_ID"),
-        "client_secret": os.getenv("GOOGLE_SECRET"),
-        "api_base_url": "https://www.googleapis.com/oauth2/v2/",
-        "client_kwargs":{ "scope": "openid email profile" },
-        "request_token_url": None,
-        "access_token_url": "https://oauth2.googleapis.com/token",
-        "authorize_url": "https://accounts.google.com/o/oauth2/auth"
+AUTH_ROLES_SYNC_AT_LOGIN = True
+
+# A mapping from LDAP/OAUTH group names to FAB roles
+AUTH_ROLES_MAPPING = {
+    "an-brk": ["brk"],
+    # "ADMIN_GROUP_NAME": ["Admin"],
+}
+
+OAUTH_PROVIDERS = [
+    {
+        "name": "azure",
+        # "whitelist": ["@company.com"],
+        "icon": "fa-windows",
+        "token_key": "access_token",
+        "remote_app": {
+            "client_id": os.getenv("AZURE_ID"),
+            "client_secret": os.getenv("AZURE_SECRET"),
+            "api_base_url": "https://login.microsoftonline.com/886f0b1c-59ba-405b-ba4a-0147eaa429c0/oauth2",
+            "client_kwargs": {
+                "scope": "User.read name preferred_username email profile upn groups",
+                "resource": os.getenv("AZURE_ID"),
+            },
+            "request_token_url": None,
+            "access_token_url": "https://login.microsoftonline.com/886f0b1c-59ba-405b-ba4a-0147eaa429c0/oauth2/token",
+            "authorize_url": "https://login.microsoftonline.com/886f0b1c-59ba-405b-ba4a-0147eaa429c0/oauth2/authorize",
+        },
     }
-}]
+]
--- superset_config.py
+++ superset_config.py
@@ -2,6 +2,39 @@ import os
 
 from flask_appbuilder.security.manager import AUTH_OAUTH
 
-
 SECRET_KEY = os.getenv("SUPERSET_SECRET_KEY")
 SQLALCHEMY_DATABASE_URI=os.getenv("SUPERSET_CONNECTION_STRING")
+
+AUTH_TYPE = AUTH_OAUTH
+AUTH_USER_REGISTRATION = True
+AUTH_USER_REGISTRATION_ROLE = "Gamma"
+
+AUTH_ROLES_SYNC_AT_LOGIN = True
+
+# A mapping from LDAP/OAUTH group names to FAB roles
+AUTH_ROLES_MAPPING = {
+    "an-brk": ["brk"],
+    # "ADMIN_GROUP_NAME": ["Admin"],
+
+}
+
+OAUTH_PROVIDERS = [
+    {
+        "name": "azure",
+        # "whitelist": ["@company.com"],
+        "icon": "fa-windows",
+        "token_key": "access_token",
+        "remote_app": {
+            "client_id": os.getenv("AZURE_ID"),
+            "client_secret": os.getenv("AZURE_SECRET"),
+            "api_base_url": "https://login.microsoftonline.com/886f0b1c-59ba-405b-ba4a-0147eaa429c0/oauth2",
+            "client_kwargs": {
+                "scope": "User.read name preferred_username email profile upn groups",
+                "resource": os.getenv("AZURE_ID"),
+            },
+            "request_token_url": None,
+            "access_token_url": "https://login.microsoftonline.com/886f0b1c-59ba-405b-ba4a-0147eaa429c0/oauth2/token",
+            "authorize_url": "https://login.microsoftonline.com/886f0b1c-59ba-405b-ba4a-0147eaa429c0/oauth2/authorize",
+        },
+    }
+]
